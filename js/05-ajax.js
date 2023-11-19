4064e85cd3e86ccc556374b0fac5d44bf3fe7c26
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-11-18 22:04:51 -0500

    Creamos el html ajax y el ejercicio 5-ajax.js, para aprender a utilizar ajax mejor

---
+++ js/05-ajax.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    //Load
+
+    /*
+    Ci-bas, on va utiliser la commande "load", avec cette commande
+    on peut prendre des données et l'intégrer sur l'élément sélectionné
+    */
+    $("#datos").load("https://reqres.in/");
+});
