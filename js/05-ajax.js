7fe5e03d713499d244898d5e1d42da908316017e
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-11-20 14:04:37 -0500

    En el ejercicio 05-ajax.js, estamos practicando GET y POST

--- js/05-ajax.js
+++ js/05-ajax.js
@@ -5,5 +5,50 @@ $(document).ready(function () {
     Ci-bas, on va utiliser la commande "load", avec cette commande
     on peut prendre des données et l'intégrer sur l'élément sélectionné
     */
-    $("#datos").load("https://reqres.in/");
+    //$("#datos").load("https://reqres.in/");
+
+    //Get
+
+    /*
+    On utilise la méthode"get" pour lier une propriété d'un objet à une fonction qui
+    sera appelée lorsqu'on accédera à la propriété. Avec "get" les données à envoyer
+    au serveur sont écrites directement dans l'url.
+    */
+
+    $.get("https://reqres.in/api/users", { page: 2 }, function (response) {
+        response.data.forEach((element, index) => {
+            $("#datos").append("<p>" + element.first_name + " " + element.last_name + "</p>");
+        });
+    });
+
+    //Post
+
+    /*
+    On utilise la méthode "post" pour envoyer des données au serveur
+    et de les récupérer. Avec la méthode "post" les paramètres URL sont écrites
+    dans la requête HTTP pour le serveur. Les paramètres ne sont donc pas visibles
+    pour les utilisateurs et la portée des requêtes "post" est illimitée.
+    */
+
+    $("#formulario").submit(function (e) {
+        e.preventDefault();
+        var usuario = {
+            name: $('input[name="first_name"]').val(),
+            web: $('input[name="web"]').val()
+        };
+        console.log(usuario);
+        $.post($(this).attr("action"), usuario, function (response) {
+            console.log(response);
+        }).done(function(){
+            alert("Usuario agregado correctamente");
+        });
+        return false;
+    });
+
+    //$.ajax
+
+    /*
+    
+    */
+
 });
