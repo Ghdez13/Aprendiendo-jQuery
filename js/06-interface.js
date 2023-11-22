55eda5385cb38d5d05a299943beed1ac7b63679a
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-11-21 21:39:17 -0500

    En el ejercicio 06-interface.js, aprendimos como utilizar algunos efectos con el uso de métodos de jQuery

--- js/06-interface.js
+++ js/06-interface.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("Hola");
 
     /*
@@ -14,32 +14,49 @@ $(document).ready(function(){
     /*
     On va utiliser la méthode "selectable" pour travailler avec les éléments qu'on sélectionne 
     */
-   //$(".lista-seleccionable").selectable();
+    //$(".lista-seleccionable").selectable();
 
-   /*
-   On va utiliser la méthode "sortable" pour déplacer l'élément de la liste où on veut
-   */
-   $(".lista-seleccionable").sortable({
-    update: function(event,ui){
-        console.log("A cambiado la lista");
-    }
-   });
+    /*
+    On va utiliser la méthode "sortable" pour déplacer l'élément de la liste où on veut
+    */
+    $(".lista-seleccionable").sortable({
+        update: function (event, ui) {
+            console.log("A cambiado la lista");
+        }
+    });
 
-   /*
-   On va utiliser la méthode "droppable", pour savoir quando quelque chose est draggable
-   dans une élément droppable.
-   */
-   $("#area").droppable({
-    drop: function(){
-        console.log("Has soltado algo dentro del área");
-    }
-   });
+    /*
+    On va utiliser la méthode "droppable", pour savoir quando quelque chose est draggable
+    dans une élément droppable.
+    */
+    $("#area").droppable({
+        drop: function () {
+            console.log("Has soltado algo dentro del área");
+        }
+    });
 
-   /*
-   
-   */
-$("#mostrar").click(function(){
-    $(".caja-efectos").toggle("shake",4000);
-});
+    // les effets
+    $("#mostrar").click(function () {
+        $(".caja-efectos").toggle("shake", 4000);
+    });
+
+    //La utilisation de la méthode "ToolTip"
+    //Il y a beaucoup de choses a faire avec la méthode tooltip, on peut visiter le site web de jQuery par plus information
+
+    $(document).tooltip();
+
+    //La méthode "dialog" on l'utilise pour lancer une fenêtre contextuelle
+    $("#lanzar-popup").click(function () {
+        $("#popup").dialog();
+
+    });
+
+    // la méthode "datepicker", on utilise cette méthode pour la creation d'un calendrier
+
+    $("#calendario").datepicker();
+
+    //La méthode "tabs" on l'utilise pour la creation d'onglets
+
+    $("#pestanas").tabs();
 
 });
